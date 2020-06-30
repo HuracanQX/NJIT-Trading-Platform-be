f1adff278b2eed8af3a8aef1f9511579c68d46a8
quanxin <1307683181@qq.com>
2020-06-30 17:07:14 +0800

    generatorPlugin dao pojo mappers

---
+++ src/main/java/com/njittp/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.njittp.dao;
+
+import com.njittp.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/njittp/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.njittp.dao;
+
+import com.njittp.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
