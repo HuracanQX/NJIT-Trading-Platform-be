7e7d838da227d64a95d4176a80a5db02e770a7d2
quanxin <1307683181@qq.com>
2020-07-03 15:50:44 +0800

    user category update

---
+++ src/main/java/com/njittp/service/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.njittp.service;
+
+import com.njittp.common.ServerResponse;
+import com.njittp.pojo.Category;
+
+import java.util.List;
+
+
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+    ServerResponse updateCategoryName(String categoryName,Integer categoryId);
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+}
