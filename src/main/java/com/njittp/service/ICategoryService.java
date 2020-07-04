80073b911325b257506fd49fa6bdc9371287331b
quanxin <1307683181@qq.com>
2020-07-04 10:33:41 +0800

    coding finish

--- src/main/java/com/njittp/service/ICategoryService.java
+++ src/main/java/com/njittp/service/ICategoryService.java
@@ -8,7 +8,10 @@ import java.util.List;
 
 public interface ICategoryService {
     ServerResponse addCategory(String categoryName, Integer parentId);
+
     ServerResponse updateCategoryName(String categoryName,Integer categoryId);
+
     ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
     ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
 }
